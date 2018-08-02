80f63551ffb0a485feb208536742fd80c22d2713
abhipanda4 <abhipanda03@gmail.com>
2018-08-02 22:15:36 +0530

    changed model architecture

--- model.py
+++ model.py
@@ -3,16 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Actor(nn.Module):
+    # Actor provides the next action to take
     def __init__(self, state_dim, action_dim, limit):
         super(Actor, self).__init__()
-        self.limit = limit
+        self.limit = torch.FloatTensor(limit)
 
-        self.fc1 = nn.Linear(state_dim, 128)
-        self.fc2 = nn.Linear(128, action_dim)
+        self.fc1 = nn.Linear(state_dim, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, action_dim)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.tanh(self.fc2(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.tanh(self.fc3(x))
         return x * self.limit
 
 
@@ -20,13 +23,18 @@ class Critic(nn.Module):
     # Critic estimates the state-value function Q(S, A)
     def __init__(self, state_dim, action_dim):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 64)
-        self.fc2 = nn.Linear(action_dim, 64)
+        self.fc1_s = nn.Linear(state_dim, 256)
+        self.fc2_s = nn.Linear(256, 64)
+        self.fc1_a = nn.Linear(action_dim, 256)
+        self.fc2_a = nn.Linear(256, 64)
         self.fc3 = nn.Linear(128, 1)
 
     def forward(self, state, action):
-        s = F.relu(self.fc1(state))
-        a = F.relu(self.fc2(action))
+        s = F.leaky_relu(self.fc1_s(state))
+        s = F.leaky_relu(self.fc2_s(s))
+
+        a = F.leaky_relu(self.fc1_a(action))
+        a = F.leaky_relu(self.fc2_a(a))
 
         x = torch.cat((s, a), dim=1)
         x = self.fc3(x)
