f72f5a8b2f67afa881e79e4f0076fdace1a16e6a
abhipanda4 <abhipanda03@gmail.com>
2018-08-02 22:16:19 +0530

    constants at one place

---
+++ constants.py
@@ -0,0 +1,13 @@
+MAX_BUFF_SIZE = 10000
+N_EPISODES = 3000
+BATCH_SIZE = 32
+GAMMA = 0.99
+TAU = 0.001
+LOG_STEPS = 20
+SAVE_STEPS = 100
+THRESHOLD_STEPS = 1000
+A_LEARNING_RATE = 1e-4
+C_LEARNING_RATE = 5e-4
+
+actor_model_path = "saved_models/actor.pth"
+critic_model_path = "saved_models/critic.pth"
