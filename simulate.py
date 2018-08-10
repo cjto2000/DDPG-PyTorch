b3864287b51a1e915284333f5551f41ff05429e4
abhipanda4 <abhipanda03@gmail.com>
2018-08-11 00:21:39 +0530

    minor changes

--- simulate.py
+++ simulate.py
@@ -2,19 +2,15 @@ import torch
 from torch.autograd import Variable
 import gym
 from model import *
-from utilities import *
+from environment import *
 
-env = gym.make('BipedalWalker-v2')
-
-state_dim = env.observation_space.shape[0]
-n_actions = env.action_space.shape[0]
-limit = float(env.action_space.high[0])
-
-agent = Actor(state_dim, n_actions, limit)
+env = Game()
+agent = Actor(env.state_dim, env.n_actions, env.limit)
 
 try:
-    agent.load_state_dict(torch.load("./saved_models/actor.pth"))
+    agent.load_state_dict(torch.load("./saved_models/best_agent.pth"))
 except:
+    print("No pretrained model found, using random model!!")
     pass
 
 is_done = False
@@ -23,8 +19,5 @@ S = env.reset()
 while not is_done:
     S = Variable(torch.FloatTensor(S))
     A = agent(S).data.numpy()
-    print(A)
-    noise = Orn_Uhlen(n_actions)
-    S_prime, R, is_done, _ = env.step(A + noise)
-    S = S_prime
-    env.render()
+    S, R, is_done = env.take_action(A)
+    env.env.render()
