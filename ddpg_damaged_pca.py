f0e4f41d00bf0c0aa0457dc468a215dda53bb3c2
Justin Ong <justinong@Justins-MacBook-Pro.local>
2021-04-29 02:06:28 -0700

    pca stuff

---
+++ ddpg_damaged_pca.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from sklearn.decomposition import PCA
+
+import os
+import numpy as np
+
+from constants import *
+from model import SuperLesionedActor, Critic, EN, ActorLastLayer
+
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+print(f"DEVICE: {device}")
+
+
+class Orn_Uhlen:
+    def __init__(self, n_actions, mu=0, theta=0.15, sigma=0.2):
+        self.n_actions = n_actions
+        self.X = np.ones(n_actions) * mu
+        self.mu = mu
+        self.sigma = sigma
+        self.theta = theta
+
+    def reset(self):
+        self.X = np.ones(self.n_actions) * self.mu
+
+    def sample(self):
+        dX = self.theta * (self.mu - self.X)
+        dX += self.sigma * np.random.randn(self.n_actions)
+        self.X += dX
+        return self.X
+
+
+class DDPG:
+    def __init__(self, env, memory, hidden_dim, include_loss=False):
+        self.env = env
+
+        n_inp = env.state_dim
+        n_out = env.n_actions
+        a_limit = env.limit
+        self.include_loss = include_loss
+
+        # self.en_net = EN(n_out).to(device)
+        # self.en_net.load_state_dict(torch.load("damaged_models/en.pth"))
+        # self.en_net.freeze_parameters()
+
+        self.actor_net = SuperLesionedActor(n_inp, n_out, hidden_dim, a_limit).to(device)
+        self.actor_net.load_weights(path="models/history08/actor.pth") # load weights
+        self.actor_net.load_en_weights(path="models/en_models/en_model_bad.pth") # replace last layer with en network weights
+        # self.actor_net.zero_weights() # zero weights of actor to damage it
+        self.actor_net.freeze_parameters() # freeze all parameters except for the CPN
+        self.critic_net = Critic(n_inp, n_out).to(device)
+        self.critic_net.load_state_dict(torch.load("models/history08/critic.pth", map_location=device))
+
+        self.actor_last_layer = ActorLastLayer(n_out, input_dim=16).to(device) # model for getting actual action to be done
+        self.actor_last_layer.load_weights(path="models/history08/actor.pth")
+
+        self.target_actor_net = SuperLesionedActor(n_inp, n_out, hidden_dim, a_limit).to(device)
+        self.target_critic_net = Critic(n_inp, n_out).to(device)
+
+        self.target_actor_net.load_state_dict(self.actor_net.state_dict())
+        self.target_critic_net.load_state_dict(self.critic_net.state_dict())
+
+        self.actor_optimizer = optim.Adam(self.actor_net.parameters(), lr=A_LEARNING_RATE)
+        self.critic_optimizer = optim.Adam(self.critic_net.parameters(), lr=C_LEARNING_RATE)
+
+        self.memory = memory
+        self.noise = Orn_Uhlen(n_out)
+
+        self.pca_time_series = np.load("tmp/pc_time_series.npy")
+        self.saved_time_series = self.avg_time_series()
+        self.reduced = np.load("tmp/reduced.npy")
+
+    def train_one_episode(self, batch_size=32):
+        S = self.env.reset()
+        is_done = False
+        R_total = 0
+        n_steps = 0
+        while not is_done and n_steps < THRESHOLD_STEPS:
+            # self.env.render()
+            S_var = Variable(torch.FloatTensor(S)).unsqueeze(0).to(device)
+            en_output, model_input = self.actor_net(S_var)
+            en_output = en_output.detach()
+            # model_input = model_input.detach()
+            # A_pred = self.actor_last_layer(model_input).detach() # get the actual action to be done
+            noise = self.noise.sample()
+            A = (en_output.data.cpu().numpy() + noise)[0][:] # use en_output instead of "actual" output
+            S_prime, R, is_done = self.env.take_action(A)
+            # store transition in replay memory (use surrogate action aka en output)
+            self.memory.add_to_memory((S, A, S_prime, R, is_done, n_steps))
+            # update the next state for next iteration
+            S = S_prime
+            R_total += R
+
+            # Training on samples drawn from replay memory
+            S_batch, A_batch, S_prime_batch, R_batch, is_done_batch, time_step_batch = self.memory.sample(batch_size)
+            # cast into variables
+            S_batch = Variable(torch.FloatTensor(S_batch)).to(device)
+            A_batch = Variable(torch.FloatTensor(A_batch)).to(device)
+            S_prime_batch = Variable(torch.FloatTensor(S_prime_batch)).to(device)
+            R_batch = Variable(torch.FloatTensor(R_batch)).to(device)
+            is_done_batch = Variable(torch.FloatTensor(is_done_batch)).to(device)
+
+            # Use en output to update critic
+            A_en_critic, model_input = self.target_actor_net(S_prime_batch)
+            Q_Spr_A = self.target_critic_net(S_prime_batch, A_en_critic).detach()
+            target_y = R_batch + GAMMA * Q_Spr_A * (1 - is_done_batch)
+            y = self.critic_net(S_batch, A_batch)
+
+            # prediction loss for critic
+            critic_loss = torch.mean(torch.pow(y - target_y, 2))
+
+            # update critic network -> Q(S, A)
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            # find actor loss, use en output
+            A_en, model_input = self.actor_net(S_batch)
+            print(S_batch)
+            print(model_input)
+            exit()
+            # prediction loss for difference between surrogate and real action
+            loss = 0
+            if self.include_loss: # include pca loss
+                indices = time_step_batch.flatten()
+                matrix = model_input.detach().numpy() @ self.get_pca_matrix(indices)
+                mask = np.identity(BATCH_SIZE)
+                ones = np.ones((BATCH_SIZE, 1))
+                matrix = np.multiply(matrix, mask)
+                pc_vector = matrix @ ones
+                target_vector = self.reduced[indices]
+                loss = np.sum((target_vector - pc_vector) ** 2)
+                loss /= 50
+
+            actor_loss = -1 * torch.mean(self.critic_net(S_batch, A_en)) + (loss if self.include_loss else 0)
+            # update actor network
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # soft update target networks
+            self.soft_update()
+
+            n_steps += 1
+
+        self.noise.reset()
+        return critic_loss, actor_loss, R_total, n_steps, loss
+
+    def soft_update(self):
+      for target, src in zip(self.target_actor_net.parameters(), self.actor_net.parameters()):
+          target.data.copy_(target.data * (1.0 - TAU) + src.data * TAU)
+
+      for target, src in zip(self.target_critic_net.parameters(), self.critic_net.parameters()):
+          target.data.copy_(target.data * (1.0 - TAU) + src.data * TAU)
+
+    def get_pca_matrix(self, time_steps):
+        return self.pca_time_series[time_steps].transpose((1, 0))
+
+    def avg_time_series(self):
+        arr = np.load("tmp/saved_time_series.npy")
+        arr = np.average(arr, axis=0)
+        print(arr.shape)
+        return arr
---
+++ train_damaged_pca.py
@@ -0,0 +1,105 @@
+import torch
+
+import json
+import pickle
+import os
+
+from environment import Game
+from replay_mem import ReplayMemoryTS
+from ddpg_damaged_pca import *
+from constants import *
+from torch.autograd import Variable
+
+start_over = True
+hidden_dim = 16
+include_loss = True
+
+# gam environment
+env = Game()
+
+# Create replay memory
+memory = ReplayMemoryTS(MAX_BUFF_SIZE, env.state_dim, env.n_actions)
+
+
+# DDPG agent
+agent = DDPG(env, memory, hidden_dim, include_loss=include_loss)
+
+def initialize_replay_mem():
+    '''
+    Initialize the replay memory.
+    '''
+    print("Initializing replay memory...")
+    S = env.reset()
+    n = 0
+    for _ in range(MAX_BUFF_SIZE):
+        S_var = Variable(torch.FloatTensor(S)).to(device)
+        A = env.sample_action()
+        S_prime, R, is_done = env.take_action(A)
+        memory.add_to_memory((S, A, S_prime, R, is_done, n))
+        if is_done or n == 999:
+            S = env.reset()
+            n = 0
+        else:
+            S = S_prime
+            n += 1
+
+
+def write_history(path, hist):
+  with open(path, "w") as f:
+    json.dump(hist, f)
+
+
+if os.path.exists("mem_init_pca.pkl") and start_over:
+  memory = pickle.load(open("mem_init_pca.pkl", "rb"))
+else:
+  initialize_replay_mem()
+  pickle.dump(memory, open("mem_init_pca.pkl", "wb"))
+
+history = {
+    "timesteps": [],
+    "running_rewards" : [],
+    "critic_loss": [],
+    "actor_loss": [],
+    "pca_loss": [],
+    "rewards": [],
+    "best_reward": 0,
+    "total_timesteps": 0
+}
+
+
+if __name__ == "__main__":
+    running_R = -100
+    best_R = -float("inf")
+    time_steps = 0
+    reward = 0
+    for i in range(1, N_EPISODES):
+        l1, l2, R, n_steps, loss = agent.train_one_episode(BATCH_SIZE)
+        time_steps += n_steps
+        running_R = 0.9 * running_R + 0.1 * R
+        reward += R
+        # TODO: maintain running rewards and losses
+        if i % LOG_STEPS == 0:
+            history["timesteps"].append(time_steps)
+            history["running_rewards"].append(round(running_R, 2))
+            history["actor_loss"].append(round(l2.item(), 2))
+            history["rewards"].append(round(reward / LOG_STEPS, 2))
+            history["total_timesteps"] += time_steps
+            history["pca_loss"].append(round(loss, 2))
+            time_steps = 0
+            write_history("history.json", history)
+            print("Episode %5d -- Running Rewards : %.5f -- Reward: %.5f -- Losses: %.5f(a) %.5f(c) %.5f(pca) -- Best Reward: %.5f -- Time steps: %d" %(i, running_R, reward / LOG_STEPS, l2, l1, loss, best_R, history["total_timesteps"]))
+            reward = 0
+        if R > best_R:
+            best_R = R
+            history["best_reward"] = round(best_R, 2)
+            torch.save(agent.actor_net.state_dict(), actor_model_path)
+            torch.save(agent.critic_net.state_dict(), critic_model_path)
+        if R > 250:
+            print("NOISE IS ZERO")
+        # if i % SAVE_STEPS == 0:
+        #     pickle.dump(memory, open("mem.pkl", "wb"))
+        #     torch.save(agent.actor_net.state_dict(), actor_save_path)
+        #     torch.save(agent.critic_net.state_dict(), critic_save_path)
+        #     torch.save(agent.target_actor_net.state_dict(), target_actor_save_path)
+        #     torch.save(agent.target_critic_net.state_dict(), target_critic_save_path)
+    print("Training complete....")
